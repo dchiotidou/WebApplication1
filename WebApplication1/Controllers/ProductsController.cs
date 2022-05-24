e8201df2369bed6439cf301084a4c641e63b8dc9
Δέσποινα Χιωτίδου <d.chiotidou@speedex.gr>
2022-05-24 09:45:28 +0300

    Add project files.

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using ODataService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ODataService.Controllers
+//namespace .Models
+{
+    public class ProductsController : Microsoft.AspNet.OData.ODataController
+    {
+        private List<Product> products = new List<Product>()
+        {
+            new Product()
+            {
+                ID = 1,
+                Name = "Bread",
+            }
+        };
+
+        public List<Product> Get()
+        {
+            return products;
+        }
+    }
+}
